9736b5934720b11c1768df20d6f7e7c450cf6104
Paul Devlin <Paul.Devlin@nihr.ac.uk>
2020-10-02 21:29:40 +0100

    Updates

---
+++ precache-manifest.efc7301e9e14fae6c32ddbb2529a72f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c2a21dd48030cbcbccf444a1a15fcb",
+    "url": "/hazard-detection-web/index.html"
+  },
+  {
+    "revision": "cb5274e55f16deef1fce",
+    "url": "/hazard-detection-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d505d611b4bf5b7f800",
+    "url": "/hazard-detection-web/static/js/2.a6dfc6ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hazard-detection-web/static/js/2.a6dfc6ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5274e55f16deef1fce",
+    "url": "/hazard-detection-web/static/js/main.95e5ab94.chunk.js"
+  },
+  {
+    "revision": "d8c439c9c688fbecb57d",
+    "url": "/hazard-detection-web/static/js/runtime-main.57249a86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hazard-detection-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
